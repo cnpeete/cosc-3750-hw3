38a3e857ad914e3b0c0494528ec8a6d13c88aa8b
cnpeete <47127460+cnpeete@users.noreply.github.com>
2025-02-10 10:25:11 -0700

    Add files via upload

---
+++ prompt.h
@@ -0,0 +1,19 @@
+/****************************
+ * prompt.h
+ * Kim Buckner
+ * COSC3750
+ * Homework 03
+ * Header for prompt function for program computing an approximation of 
+ * the constant e.
+ ***************************/
+
+#ifndef PROMPT_H
+#define PROMPT_H
+
+#include<stdio.h>
+#include<errno.h>
+
+extern int  prompt(void);
+
+#endif
+
